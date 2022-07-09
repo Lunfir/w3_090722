3ff8346981aa6e666c0532f788693f201e943cc0
Lunfir <lunfirok@gmail.com>
2022-07-09 18:35:36 +0300

    Ptrs to functions: pass ptrs to functions

--- funcPtr.c
+++ funcPtr.c
@@ -2,28 +2,55 @@
 
 #define SIZE 3
 
-void cat() {
-    printf("meow\n");
+void cat(int food) {
+    if (food < 2) {
+         printf("MEOW MEOW MEOW\n");
+    }
+    else {
+        printf("meow\n");
+    }
 }
 
-void dog() {
+void dog(int food) {
     printf("bark\n");
 }
 
-void snake() {
+void snake(int food) {
     printf("fsssss\n");
 }
 
+void feed(void(*ptrFunc)(int), int food) {
+    if (food < 10) {
+        printf("Not enough food\n");
+        
+        return;
+    }
+
+    ptrFunc(food);
+}
+
 int main() {
-    void(*animals[])() = { &cat, &dog, &snake };
+    void(*animals[])(int) = { &cat, &dog, &snake };
 
     // animals[0] = &cat;
     // animals[1] = &dog;
     // animals[2] = &snake;
 
     for (int i = 0; i < SIZE; i++) {
-        animals[i]();
+        feed(animals[i], 42);
     }
 
     return 0;
 }
+
+// TODO: try to add sorting function  to bubble sort
+// int criteria(int a, int b) {
+//     if ( a < b && (a % 2 == 0))
+//     {
+//         return 1;
+//     }
+
+//     return 0;
+// }
+//  // pseudo-code
+// void quickSort(int[], *criteria)
