b1239bb4fc9c9c0a9ec7b9eb799a1447daca237c
Lunfir <lunfirok@gmail.com>
2022-07-10 13:53:43 +0300

    Templates: basics

--- cpp/templates.cpp
+++ cpp/templates.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
 
 
-int sum(int a, int b)
+template <typename T>
+T sum(T a, T b)
 {
     std::cout << __PRETTY_FUNCTION__ << "   ";
 
     return a + b;
 }
 
-float sum(float a, float b)
-{
-    std::cout << __PRETTY_FUNCTION__ << "   ";
-
-    return a + b;
-}
 
 int main()
 {   
@@ -21,9 +16,11 @@ int main()
     // std::cin >> num;
     // std::cout << "num: " << num << std::endl;
 
-    std::cout << sum(5, 4) << std::endl;
-    std::cout << sum(5.12f, 4.23f) << std::endl;
+    std::cout << sum<int>(5, 4) << std::endl;
+    std::cout << sum<float>(5.12, 4.23) << std::endl;
+    std::cout << sum<double>(5.12, 4.23) << std::endl;
 
+    // std::cout << sum("abc", "bcs") << std::endl; // "abc" + "bcs" = "abcbcs"; const char* + const char*
 
     return 0;
 }
